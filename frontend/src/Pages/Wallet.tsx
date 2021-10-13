cd169593baa8044d9415913959c5a0d1fbd0c6c3
flomllr <contact@flomllr.com>
2021-10-13 16:03:24 +0200

    Add cardgrid to wallet

--- frontend/src/Pages/Wallet.tsx
+++ frontend/src/Pages/Wallet.tsx
@@ -1,5 +1,10 @@
+import { CardGrid } from '../Components/CardGrid';
 import { VectorBackground } from '../Components/VectorBackground';
 
 export const Wallet: React.FC = () => {
-  return <VectorBackground title={'Wallet'}></VectorBackground>;
+  return (
+    <VectorBackground title={'Wallet'}>
+      <CardGrid />
+    </VectorBackground>
+  );
 };
