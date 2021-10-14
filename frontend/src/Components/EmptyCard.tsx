41512f9c1205294fe85d72ea6ca2f0ee2d6e61b1
Jan K <jan.krusnik@tum.de>
2021-10-14 21:19:00 +0200

    Add ChairSection and EmptyCard

---
+++ frontend/src/Components/EmptyCard.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import user_icon from '../assets/user_icon.png';
+
+export const EmptyCard: React.FC<{name: string}> = ({ name }) => {
+  return (
+    <Container>
+      <Image src={user_icon} />
+      <NameTitle>{name}</NameTitle>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: #c4c4c4;
+  margin: 50px 0px;
+  width: 100%;
+  height: 250px;
+  border-radius: 16px;
+  box-shadow: 5px 5px 10px 6px rgba(0,0,0,0.25);
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr;
+  text-align: center;
+`;
+
+const NameTitle = styled.h3`
+  align-self: start;
+`;
+
+const Image = styled.img`
+  margin-top: 10px;
+  justify-self: center;
+  width: auto;
+  max-height: 60%;
+`;
--- frontend/src/Pages/Collection.tsx
+++ frontend/src/Pages/Collection.tsx
@@ -1,10 +1,63 @@
+import styled from 'styled-components';
 import { CardGrid } from '../Components/CardGrid';
+import { ChairSection } from '../Components/ChairSection';
 import { VectorBackground } from '../Components/VectorBackground';
+import { NFT } from '../types';
 
 export const Collection: React.FC = () => {
   return (
     <VectorBackground title={'Collection'}>
-      <CardGrid nfts={[]} />
+      <Container>
+        <Sidebar>
+          <UniLink href="#">TUM</UniLink>
+          <Divider />
+          <UniLink href="#">LMU</UniLink>
+        </Sidebar>
+        <Content>
+          <UniTitle>University Chairs</UniTitle>
+          <ChairSection professors={[]} asisstants={[]} />
+        </Content>
+      </Container>
     </VectorBackground>
   );
 };
+
+const Sidebar = styled.div`
+  height: auto; /* Full-height: remove this if you want "auto" height */
+  top: 0; /* Stay at the top */
+  left: 0;
+  overflow-x: hidden; /* Disable horizontal scroll */
+  text-align: left;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 9fr;
+  grid-gap: 40px;
+`;
+
+const Content = styled.div`
+  width: 100%;
+`;
+
+const UniLink = styled.a`
+  padding: 6px 8px;
+  text-decoration: none;
+  font-size: 20px;
+  color: #000000;
+  display: block;
+  transition: ease-in-out .2s;
+  :hover {
+    color: #ad59f7;
+  }
+`;
+
+const Divider = styled.hr`
+  border: 0;
+  border-top: 2px solid #bbb;
+  border-radius: 5px;
+`
+
+const UniTitle = styled.h1`
+  padding-bottom: 30px;
+`
