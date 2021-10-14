bcac49352d2855f537df12ea9a64bf275ace7a23
Jan K <jan.krusnik@tum.de>
2021-10-14 21:37:28 +0200

    Fix EmptyCard

--- frontend/src/Components/EmptyCard.tsx
+++ frontend/src/Components/EmptyCard.tsx
@@ -17,7 +17,6 @@ const Container = styled.div`
   border-radius: 16px;
   box-shadow: 5px 5px 10px 6px rgba(0,0,0,0.25);
   position: relative;
-  display: grid;
   grid-template-columns: 1fr;
   text-align: center;
 `;
