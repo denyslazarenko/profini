f6bd5395cccd34f153e53a0098d1ff61703a12eb
Nick Stracke <nick.stracke@web.de>
2021-10-14 15:34:09 +0200

    add first versions of contract

--- contracts/BoosterPack.sol
+++ contracts/BoosterPack.sol
@@ -5,7 +5,8 @@ import "./Profini.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BoosterPack is Ownable {
-    uint256 constant PRICE_PER_BOOSTER = 0.001 ether;
+    uint256 constant PRICE_PER_BOOSTER = 0.00001 ether;
+    uint256 constant CARDS_PER_BOOSTER = 3;
     mapping(string => uint256) private cardsClaimed;
     uint256[] private tokenIds;
     address private contractAddress;
@@ -15,11 +16,19 @@ contract BoosterPack is Ownable {
         contractAddress = _contractAddress;
     }
 
-    function randomNumber(uint256 max) internal returns (uint256) {
-        return max;
+    function randomNumber(uint256 max) internal view returns (uint256) {
+        bytes memory b = abi.encodePacked(
+            blockhash(block.number),
+            msg.sender,
+            max
+        );
+        bytes32 h = keccak256(b);
+        uint256 rndInt = uint256(h);
+
+        return ((rndInt % max) - 1) + 1;
     }
 
-    function drawPack() public payable {
+    function drawPack() public payable returns (uint256[] memory) {
         require(msg.value >= PRICE_PER_BOOSTER);
 
         Profini profini = Profini(contractAddress);
@@ -29,7 +38,9 @@ contract BoosterPack is Ownable {
             accounts[i] = owner();
         }
 
-        for (uint256 i = 0; i < 3; i++) {
+        uint256[] memory drawnCards = new uint256[](CARDS_PER_BOOSTER);
+
+        for (uint256 i = 0; i < CARDS_PER_BOOSTER; i++) {
             uint256[] memory balances = profini.balanceOfBatch(
                 accounts,
                 tokenIds
@@ -52,6 +63,25 @@ contract BoosterPack is Ownable {
             }
 
             uint256 rnd = randomNumber(totalBalance);
+
+            for (uint256 j = 0; j < tokenRanges.length; j++) {
+                uint256[] memory range = tokenRanges[j];
+                if (rnd >= range[0] && rnd <= range[1]) {
+                    uint256 drawnCard = j + 1;
+                    drawnCards[i] = drawnCard;
+
+                    profini.safeTransferFrom(
+                        owner(),
+                        msg.sender,
+                        drawnCard,
+                        1,
+                        ""
+                    );
+                    break;
+                }
+            }
         }
+
+        return drawnCards;
     }
 }
