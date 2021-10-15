e9e94d35e20ac19a1910252b521e3ddf2191eb77
Nick Stracke <nick.stracke@web.de>
2021-10-15 17:33:07 +0200

    increase faucet amount

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY || "NaN";
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
 const provider: JsonRpcProvider = new JsonRpcProvider(
-    "https://polygon-mumbai.infura.io/v3/b9bdb6b417c14d7d853913f3a1559e22");
+  "https://polygon-mumbai.infura.io/v3/b9bdb6b417c14d7d853913f3a1559e22");
 
 const wallet: Wallet = new Wallet(PRIVATE_KEY, provider);
 const contract = new ethers.Contract(CONTRACT_ADDRESS, NFT_ABI, provider);
@@ -25,39 +25,39 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/:addr', (req: Request, res: Response) => {
-    const receiveAddr = req.params.addr;
-    console.log(utils.getAddress(receiveAddr));
-    let tx = {
-        to: receiveAddr,
-        value: utils.parseEther('0.0001')
-    };
+  const receiveAddr = req.params.addr;
+  console.log(utils.getAddress(receiveAddr));
+  let tx = {
+    to: receiveAddr,
+    value: utils.parseEther('0.05')
+  };
 
-    let sendPromise = wallet.sendTransaction(tx);
-    sendPromise
+  let sendPromise = wallet.sendTransaction(tx);
+  sendPromise
     .then((tx) => {
-        console.log(tx);
-        res.sendStatus(200);
+      console.log(tx);
+      res.sendStatus(200);
     })
     .catch(err => console.log(err));
 });
 
 app.get('/contract/tokenIDs', (req: Request, res: Response) => {
-    let tokenPromise = contract.tokenIDs();
-    tokenPromise
+  let tokenPromise = contract.tokenIDs();
+  tokenPromise
     .then((value: any) => {
-        console.log(value);
-        res.send(value);
+      console.log(value);
+      res.send(value);
     })
     .catch((err: any) => console.log(err));
 
 });
 
 app.get('/contract/uris', (req: Request, res: Response) => {
-    let urisPromise = contract.uris();
-    urisPromise
+  let urisPromise = contract.uris();
+  urisPromise
     .then((value: any) => {
-        console.log(value);
-        res.send(value);
+      console.log(value);
+      res.send(value);
     })
     .catch((err: any) => console.log(err));
 });
